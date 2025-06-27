f33a428787e47379574e32235cca954b51615293
dmitrii-syazan <da.mingalev@gmail.com>
2025-06-27 20:23:03 +0800

    update safe vault

--- contracts/SafeVault.sol
+++ contracts/SafeVault.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.27;
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract SafeVault is AccessControl, ReentrancyGuard {
     using SafeERC20 for IERC20;
@@ -12,6 +13,7 @@ contract SafeVault is AccessControl, ReentrancyGuard {
     bytes32 public constant BRIDGE_ROLE = keccak256("BRIDGE_ROLE");
 
     IERC20 public immutable usdc;
+    AggregatorV3Interface public priceFeed;
 
     mapping(address => uint256) public _balances;
 
@@ -22,9 +24,10 @@ contract SafeVault is AccessControl, ReentrancyGuard {
     error ZeroAmount();
     error InsufficientBalance(uint256 available, uint256 required);
 
-    constructor(address defaultAdmin, address usdcAddress) {
+    constructor(address defaultAdmin, address usdcAddress, address priceFeedAddress) {
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
         usdc = IERC20(usdcAddress);
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     function processDeposit(address user, uint256 amount) external onlyRole(BRIDGE_ROLE) {
@@ -35,7 +38,7 @@ contract SafeVault is AccessControl, ReentrancyGuard {
         emit Deposit(user, amount);
     }
 
-    function requestWithdraw(address user, uint256 amount) external onlyRole(DEFAULT_ADMIN_ROLE) nonReentrant {
+    function requestWithdraw(address user, uint256 amount) external onlyRole(BRIDGE_ROLE) nonReentrant {
         if (user == address(0)) revert ZeroAddress();
         if (amount == 0) revert ZeroAmount();
         uint256 bal = _balances[user];
@@ -47,5 +50,24 @@ contract SafeVault is AccessControl, ReentrancyGuard {
 
     function getBalance(address user) external view returns (uint256) {
         return _balances[user];
+    }
+
+    function getLatestPrice() public view returns (int256) {
+        // Chainlink Data Feed
+        (
+            ,
+            int256 price,
+            ,
+            ,
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    function getBalanceInUSD(address user) external view returns (uint256) {
+        uint256 balance = _balances[user];
+        int256 price = getLatestPrice();
+        require(price > 0, "Invalid price");
+
+        return (balance * uint256(price)) / 1e8;
     }
 }
